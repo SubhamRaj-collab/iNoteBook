cf0c3b7cf5cb527db7d2cc8bfbde0069eea08c7d
Subham Raj <glorious10info@gmail.com>
2023-04-18 12:00:42 +0530

    backend endpoints created for explanation

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello Raj!')
 })
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
